e532723cd652cabf5f08f3cd21016588f8f07a68
203455 <77992738+203455@users.noreply.github.com>
2022-03-10 19:12:11 -0600

    First Commit

---
+++ src/main/java/com/example/CashFlowBack/CashFlowBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.CashFlowBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CashFlowBack/CashFlowBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CashFlowBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
