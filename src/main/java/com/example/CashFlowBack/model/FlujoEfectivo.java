3a41c7ca8d88579e38cc14055d4c233b76467c4f
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-03-14 00:28:51 -0600

    create new FlujoEfectivo model

--- src/main/java/com/example/CashFlowBack/model/FlujoEfectivo.java
+++ src/main/java/com/example/CashFlowBack/model/FlujoEfectivo.java
@@ -1,5 +1,7 @@
 package com.example.CashFlowBack.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,47 +12,57 @@ public class FlujoEfectivo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_flujo;
 
-    @Column(name = "AccountType")
-    private String accountType;
+    @Column(name = "FlowType")
+    private String flowType;
 
-    @Column(name = "NWeek")
-    private int nWeek;
+    @Column(name = "Date")
+    private int date;
 
-    @Column(name = "BusinessName")
-    private String businessName;
+    @Column(name = "Description")
+    private String description;
 
     @Column(name = "Amount")
     private double amount;
 
-    public FlujoEfectivo(int id_flujo, String accountType, int nWeek, String businessName, double amount) {
+    @JsonBackReference
+    @ManyToOne (fetch = FetchType.LAZY, optional = false)
+    @JoinColumn (name = "id_categoria", nullable = false)
+    private Categoria categoria;
+
+
+    public FlujoEfectivo(int id_flujo, String flowType, int date, String description, double amount, Categoria categoria) {
         this.id_flujo = id_flujo;
-        this.accountType = accountType;
-        this.nWeek = nWeek;
-        this.businessName = businessName;
+        this.flowType = flowType;
+        this.date = date;
+        this.description = description;
         this.amount = amount;
+        this.categoria = categoria;
     }
 
     public FlujoEfectivo() {
-
     }
 
     public int getId_flujo() { return id_flujo; }
 
     public void setId_flujo(int id_flujo) { this.id_flujo = id_flujo; }
 
-    public String getAccountType() { return accountType; }
+    public String getFlowType() { return flowType; }
 
-    public void setAccountType(String accountType) { this.accountType = accountType; }
+    public void setFlowType(String flowType) { this.flowType = flowType; }
 
-    public int getnWeek() { return nWeek; }
+    public int getDate() { return date; }
 
-    public void setnWeek(int nWeek) { this.nWeek = nWeek; }
+    public void setDate(int date) { this.date = date; }
 
-    public String getBusinessName() { return businessName; }
+    public String getDescription() { return description; }
 
-    public void setBusinessName(String businessName) { this.businessName = businessName; }
+    public void setDescription(String description) { this.description = description; }
 
     public double getAmount() { return amount; }
 
     public void setAmount(double amount) { this.amount = amount; }
+
+    public Categoria getCategoria() { return categoria; }
+
+    public void setCategoria(Categoria categoria) { this.categoria = categoria; }
 }
