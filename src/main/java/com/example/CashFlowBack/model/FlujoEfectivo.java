ff00c8eccc8582289d8c0a7f464544a47ed5159b
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-03-17 00:14:54 -0600

    change name reference to table on FlujoEfectivo models

--- src/main/java/com/example/CashFlowBack/model/FlujoEfectivo.java
+++ src/main/java/com/example/CashFlowBack/model/FlujoEfectivo.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "flujoEfectivo")
+@Table(name = "flujo_efectivo")
 public class FlujoEfectivo {
 
     @Id
