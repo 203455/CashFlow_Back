6819712edc5879016027af158e82b85e7352a59e
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-03-18 17:08:47 -0600

    create Banco controller

---
+++ src/main/java/com/example/CashFlowBack/controller/BancoController.java
@@ -0,0 +1,36 @@
+package com.example.CashFlowBack.controller;
+
+import com.example.CashFlowBack.model.Banco;
+import com.example.CashFlowBack.repository.BancoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class BancoController {
+    @Autowired
+    BancoRepository bancoRepository;
+
+    @GetMapping(value = "/banco/")
+    public List<Banco> getAll(){
+        List<Banco> response = bancoRepository.findAll();
+
+        if(response!=null){
+            return  response;
+        }else {
+            return null;
+        }
+    }
+
+    @PostMapping(value = "/banco/save")
+    public Banco saveIndicador(@RequestBody Banco banco){
+        return  bancoRepository.save(banco);
+    }
+
+    @GetMapping(value = "/banco/{mes}")
+    public List<Banco> getByMes(@PathVariable("mes") String mes){
+        return bancoRepository.findAllByMes(mes);
+    }
+}
