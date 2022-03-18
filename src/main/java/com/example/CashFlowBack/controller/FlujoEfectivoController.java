2ecc13578b541cfb4753f459f64416e7cf79c9a9
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-03-18 17:10:41 -0600

    change imports and add the crossorigin to FlujoEfectivoController

--- src/main/java/com/example/CashFlowBack/controller/FlujoEfectivoController.java
+++ src/main/java/com/example/CashFlowBack/controller/FlujoEfectivoController.java
@@ -3,14 +3,12 @@ package com.example.CashFlowBack.controller;
 import com.example.CashFlowBack.model.FlujoEfectivo;
 import com.example.CashFlowBack.repository.FlujoEfectivoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class FlujoEfectivoController {
 
     @Autowired
