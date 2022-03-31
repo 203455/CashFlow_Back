b91a50c78541fd2a8c89360bb3b6fe4be8ed6419
203455 <77992738+203455@users.noreply.github.com>
2022-03-31 12:22:11 -0600

    Add Update Categoria

--- src/main/java/com/example/CashFlowBack/controller/CategoriaController.java
+++ src/main/java/com/example/CashFlowBack/controller/CategoriaController.java
@@ -3,11 +3,14 @@ package com.example.CashFlowBack.controller;
 import com.example.CashFlowBack.model.Categoria;
 import com.example.CashFlowBack.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import java.lang.Boolean;
 
 import java.util.List;
 
 @RestController
+@RequestMapping
 @CrossOrigin(origins = "http://localhost:3000")
 public class CategoriaController {
     @Autowired
@@ -26,5 +29,25 @@ public class CategoriaController {
     @PostMapping(value ="/categoria/add")
     public Categoria addCategoria(@RequestBody Categoria categoria){
         return categoriaRepository.save(categoria);
+    }
+
+    @PutMapping(path = "/categoria/update/{id}", consumes = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    },
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public boolean updateCategoria(@PathVariable int id, @RequestBody Categoria categoria){
+        Categoria c = categoriaRepository.findByIdCategoria(id);
+        if(c != null){
+
+            c.setSubCategoria(categoria.getSubCategoria());
+            categoriaRepository.save(c);
+            return  true;
+        }else{
+            return  false;
+        }
     }
 }
